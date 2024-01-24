b9cccfda4eb82ae520c2d95f20edec92b9fa438b
Jenya <113167825+Genya10@users.noreply.github.com>
2024-01-24 12:26:31 +0200

    create ValidateRequest.js

---
+++ server/utils/ValidateRequest.js
@@ -0,0 +1,15 @@
+import { ErrorUtils,Unprocessable } from "./Errors";
+
+export default async (req, res,next,schema)=>{
+    try{
+        if(schema){
+            await schema.validate(req);
+        }
+        return next();
+    }catch({path,errors}){
+        return ErrorUtils.catchError(
+            res,
+            new Unprocessable(JSON.stringify({path,errors}))
+        );
+    }
+};
