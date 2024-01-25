755e8c605c89dccc7377d4672f8b72194c30b103
Jenya <113167825+Genya10@users.noreply.github.com>
2024-01-25 11:56:19 +0200

    create queries.sql

---
+++ server/queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role SMALLINT NOT NULL
+);
+
+CREATE TABLE refresh_session(
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    refresh_token VARCHAR(400) NOT NULL,
+    finger_print VARCHAR(32) NOT NULL
+);
+
+SELECT * FROM users;
+SELECT * FROM refresh_sessions;
