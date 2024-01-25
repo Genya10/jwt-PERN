39b24d074cc9326597d4ec2596958f89b865507e
Jenya <113167825+Genya10@users.noreply.github.com>
2024-01-25 12:32:54 +0200

    add some changes and create app.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import style from "./app.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.wrapper}>
     <h1>JWT PERN</h1>
     </div>
   );
